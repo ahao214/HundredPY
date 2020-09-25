9e9bd12796c8dac728f89f886b440c415bc8f6a1
ahao214 <haochen214@163.com>
2020-09-25 14:04:11 +0800

    repr()

---
+++ CrazyPython/02/to_string.py
@@ -0,0 +1,15 @@
+s1 = "这本书的价格是："
+p = 99.8
+
+# 使用str()将数值转化为字符串
+print("使用str():" + s1 + str(p))
+
+# 使用repr()将数值转化为字符串
+print("使用repr():" + s1 + repr(p))
+
+
+sr = "I will play basketball"
+print(sr)
+print(repr(sr))
+
+
