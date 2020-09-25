21cb792793e0455a91b69c93be18e3109c3b354d
ahao214 <haochen214@163.com>
2020-09-25 14:40:15 +0800

    转义字符

---
+++ CrazyPython/02/Paraphrase_test.py
@@ -0,0 +1,22 @@
+# 转义字符
+# \b    退格符
+# \n    换行符
+# \r    回车符
+# \t    制表符
+# \"    双引号
+# \'    单引号
+# \\    反斜线
+
+one = "hello\nworld\ngood\nmorning"
+print(one)
+
+two ="名称\t\t单价\t数量\t总价"
+three ="Python学习\t78\t\t1\t\t78"
+print(two)
+print(three)
+
+
+
+
+
+
