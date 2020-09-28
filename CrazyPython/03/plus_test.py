bce6a9d80823e1fe01e9d711c3796fb213061429
ahao214 <haochen214@163.com>
2020-09-28 15:37:24 +0800

    加法和乘法

---
+++ CrazyPython/03/plus_test.py
@@ -0,0 +1,21 @@
+# 加法
+
+a_tuple = ("crazy", "python", "js", "vue")
+b_tuple = ("C++", "C#")
+# 计算元祖相加
+print(a_tuple+b_tuple)
+
+a_list = ['a', 'b', 'c']
+b_list = [1, 2, 3]
+# 计算列表相加
+print(a_list + b_list)
+print(a_list + ['fff'])
+
+# 元祖和列表的乘法
+print(a_tuple * 3)
+print(a_list * 3)
+
+
+
+
+
