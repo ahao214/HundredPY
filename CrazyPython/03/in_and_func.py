bf56cc17ffe3c009c3612ca58266483eb3007cfb
ahao214 <haochen214@163.com>
2020-09-28 15:42:20 +0800

    in运算和长度、最大值、最小值

---
+++ CrazyPython/03/in_and_func.py
@@ -0,0 +1,18 @@
+# in 运算符
+print("in 运算符")
+a_tuple = ("c", "it", "vue", 20, 1.2)
+print(20 in a_tuple)
+print('c' in a_tuple)
+print('it' not in a_tuple)
+
+print("元祖的长度、最大值、最小值")
+b_tuple = (10, 12, 20, 45, -98)
+print(len(b_tuple))
+print(max(b_tuple))
+print(min(b_tuple))
+
+print("列表的长度、最大值、最小值")
+b_list = ["a", "abc", "apple", "blue", "red"]
+print(len(b_list))
+print(max(b_list))
+print(min(b_list))
