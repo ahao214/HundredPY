7bc4b7a76b6dbb31a720df3dc54893b6b859e46e
ahao214 <haochen214@163.com>
2020-09-29 16:00:04 +0800

    删除列表元素

--- CrazyPython/03/list_opera_test.py
+++ CrazyPython/03/list_opera_test.py
@@ -35,3 +35,33 @@ c_list.insert(3, "d")
 print(c_list)
 
 
+
+print("删除列表元素")
+a_list =["crazy", 20, -10, ("a", "b"), "ahao"]
+# 删除第3个元素
+del a_list[2]
+print(a_list)
+# 删除第2个到第4个元素
+del a_list[1:3]
+print(a_list)
+
+b_list = list(range(1,10))
+print(b_list)
+# 删除第3个到倒数第2个元素，间隔是2
+del b_list[2:-2:2]
+print(b_list)
+# 删除第3个到第5个元素，间隔是2
+del b_list[2:4:2]
+print(b_list)
+
+
+print("remove方法")
+c_list =["crazy", 20, -10, ("a", "b"), "ahao"]
+print(c_list)
+c_list.remove(20)
+print(c_list)
+
+print("clear方法")
+c_list.clear()
+print(c_list)
+
