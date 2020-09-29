117d932f4a83d52c537b6cb17de7a4051621e064
ahao214 <haochen214@163.com>
2020-09-29 15:43:16 +0800

    列表和元祖的转换

---
+++ CrazyPython/03/list_test.py
@@ -0,0 +1,21 @@
+# 列表
+a_tuple = ("crazy", 1,2, "hello")
+# 将元祖转换为列表
+a_list = list(a_tuple)
+print(a_list)
+
+# 使用range函数创建区间对象
+a_range = range(1, 5)
+# 将区间转化为列表
+b_list = list(a_range)
+print(b_list)
+
+# 创建区间的时候指定步长
+c_list = list(range(1, 10, 2))
+print(c_list)
+
+
+
+
+
+
---
+++ CrazyPython/03/tuple_test.py
@@ -0,0 +1,12 @@
+a_list = ["c", "b", "d"]
+# 将列表转化为元祖
+a_tuple = tuple(a_list)
+print(a_tuple)
+
+# 将区间转化为元祖
+a_range = range(1,5)
+b_tuple = tuple(a_tuple)
+print(b_tuple)
+
+print(tuple(range(1, 10, 2)))
+
