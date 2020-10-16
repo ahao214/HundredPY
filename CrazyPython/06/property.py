7c7b7695016c2ecd2ca13e6290d40ccced0c0d1a
ahao214 <haochen214@163.com>
2020-10-16 14:17:42 +0800

    使用property函数定义属性

--- CrazyPython/06/property.py
+++ CrazyPython/06/property.py
@@ -38,9 +38,39 @@ rect.size = 9, 7
 print(rect.width)
 print(rect.height)
 
+
 # # 删除rect的size属性
 # del rect.size
 #
 # # 访问rect的width、height实例变量
 # print(rect.width)
 # print(rect.height)
+
+
+class User:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def getfullname(self):
+        return self.first + ',' + self.last
+
+    def setfullname(self, fullname):
+        first_last = fullname.rsplit(',')
+        self.first = first_last[0]
+        self.last = first_last[1]
+
+    # 使用property()函数定义fullname属性，只传入两个参数
+    # 该属性是一个读写属性，但不能删除
+    fullname = property(getfullname, setfullname)
+
+
+u = User('悟空', '孙')
+
+# 访问fullname属性
+print(u.fullname)
+
+# 对fullname属性赋值
+u.fullname = '八戒,猪'
+print(u.first)
+print(u.last)
