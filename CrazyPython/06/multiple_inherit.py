7fc35445919a6517897fa4654cfa76a0e9e9da49
ahao214 <haochen214@163.com>
2020-10-16 15:36:20 +0800

    多继承

---
+++ CrazyPython/06/multiple_inherit.py
@@ -0,0 +1,29 @@
+# 多继承
+
+
+class Item:
+    def info(self):
+        print("Item中方法：", '这是一个商品')
+
+
+class Product:
+    def info(self):
+        print("Product中方法：", '这是一个工业产品')
+
+
+class Mouse(Item, Product):
+    pass
+
+class Keyboard(Product, Item):
+    pass
+
+m = Mouse()
+m.info()
+
+k = Keyboard()
+k.info()
+
+print("-----------")
+
+
+
