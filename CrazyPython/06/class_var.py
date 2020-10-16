7e3bd5e2ae6e220ad6e0cd0dec076181ca2bd501
ahao214 <haochen214@163.com>
2020-10-16 13:43:09 +0800

    成员变量

--- CrazyPython/06/class_var.py
+++ CrazyPython/06/class_var.py
@@ -39,5 +39,37 @@ print(rc.item)
 print(rc.date)
 rc.info()
 
+print('修改Record两个类变量之后')
+# 修改Record两个类变量
+rc.item = '键盘'
+rc.date = '2020-09-10'
+rc.info()
+
+print('--------')
+
+
+class Inventory:
+    # 定义两个类变量
+    item = '鼠标'
+    quantity = 2000
+
+    # 定义实例方法
+    def change(self, item, quantity):
+        # 下面赋值语句不是对类变量赋值，而是定义新的实例变量
+        self.item = item
+        self.quantity = quantity
+
+
+# 创建Inventory对象
+iv = Inventory()
+iv.change('显示器', 5000)
+# 访问iv的item和quantity实例变量
+print(iv.item)
+print(iv.quantity)
+# 访问Inventory的item和quantity类变量
+print(Inventory.item)
+print(Inventory.quantity)
+
+print('--------')
 
 
