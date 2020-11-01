54a27b0794ea63e60b8555f1de26fb72d849ab36
ahao214 <haochen214@163.com>
2020-11-01 17:20:03 +0800

    type函数

--- CrazyPython/06/type_class.py
+++ CrazyPython/06/type_class.py
@@ -3,7 +3,10 @@ def fn(self):
 
 
 # 使用type定义Dog类
-Dog = type('Dog', (object,), dict(walk=fn, age = 6))
+# 第一个参数：创建的类名
+# 第二个参数：该类继承的父类集合
+# 第三个参数：该字典对象为该类绑定的类变量和方法
+Dog = type('Dog', (object,), dict(walk=fn, age=6))
 
 # 创建Dog对象
 d = Dog()
