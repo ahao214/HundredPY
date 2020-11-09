6c5ccba965ddb9c9f87fabd977e72b02ca12c615
ahao214 <haochen214@163.com>
2020-11-09 22:24:39 +0800

    异常处理

---
+++ CrazyPython/07/gobang.py
@@ -0,0 +1,18 @@
+import sys
+try:
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    c = a / b
+    print("你输入的两个数相除的结果是：", c)
+
+except IndexError:
+    print("索引错误：运行程序时输入的参数个数不对")
+
+except ValueError:
+    print("数值错误：程序只能接受整数参数")
+
+except ArithmeticError:
+    print("算术错误")
+
+except Exception:
+    print("未知异常")
