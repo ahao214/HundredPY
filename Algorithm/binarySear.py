5861a188be815a10a9fd40356486d055c2a017f6
ahao214 <haochen214@163.com>
2021-08-14 21:45:01 +0800

    二分查找

--- Algorithm/binarySear.py
+++ Algorithm/binarySear.py
@@ -8,7 +8,7 @@ def binary_search(li, val):
     left = 0
     right = len(li) - 1
     while left <= right:
-        mid = (left + right) / 2
+        mid = (left + right) // 2
         if li[mid] == val:
             return mid
         elif li[mid] > val:  # 带查找的值在mid的左侧
@@ -17,3 +17,7 @@ def binary_search(li, val):
             left = mid + 1
     else:
         return None
+
+
+li = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(binary_search(li, 4))
