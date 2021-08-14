f4eacc0f041c50286c26593b35d1aa5c68a48704
ahao214 <haochen214@163.com>
2021-08-14 22:19:31 +0800

    冒泡排序

--- Algorithm/bubble_sort.py
+++ Algorithm/bubble_sort.py
@@ -3,10 +3,14 @@
 
 def bubbleSort(li):
     for i in range(len(li) - 1):  # 第i趟
+        exchange = False
         for j in range(len(li) - i - 1):
             if li[j] > li[j + 1]:
                 li[j], li[j + 1] = li[j + 1], li[j]
+                exchange = True
         print(li)
+        if not exchange:
+            return
 
 
 li = [2, 3, 9, 1, 8, 4, 6, 7, 0]
