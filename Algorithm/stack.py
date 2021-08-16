4f2e105bbc3c07217008524e2bb930f82fc554ff
ahao214 <haochen214@163.com>
2021-08-17 02:51:18 +0800

    括号匹配问题

--- Algorithm/stack.py
+++ Algorithm/stack.py
@@ -14,9 +14,36 @@ class Stack:
         else:
             return None
 
+    def isEmpty(self):
+        return len(self.stack) == 0
 
-stack = Stack()
-stack.push(1)
-stack.push(2)
-stack.push(4)
-print(stack.pop())
+
+# stack = Stack()
+# stack.push(1)
+# stack.push(2)
+# stack.push(4)
+# print(stack.pop())
+
+
+def brace_match(s):
+    match = {'}': '{', ']': '[', ')': '('}
+    stack = Stack()
+    for ch in s:
+        if ch in {'(', '[', '{'}:
+            stack.push(ch)
+        else:
+            if stack.isEmpty():
+                return False
+            elif stack.get_top() == match[ch]:
+                stack.pop()
+            else:
+                return False
+
+    if stack.isEmpty():
+        return True
+    else:
+        return False
+
+
+print(brace_match('[][](){}{}'))
+print(brace_match('[[[](()]]][[{{}}}()'))
