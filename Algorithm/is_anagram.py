b06a99ef2577f87437b24747880bd668258b805a
ahao214 <haochen214@163.com>
2021-08-17 00:50:02 +0800

    判断两个字符串是否有相同的字母组成

---
+++ Algorithm/is_anagram.py
@@ -0,0 +1,22 @@
+# 判断两个字符串是否有相同的字母组成
+
+def isAnagram(s, t):
+    ss = list(s)
+    tt = list(t)
+    ss.sort()
+    tt.sort()
+    return ss == tt
+
+def isAnagram1(s, t):
+    dict1={}
+    dict2={}
+    for ch in s:
+        dict1[ch]=dict1.get(ch,0)+1
+    for ch in t:
+        dict2[ch]=dict2.get(ch,0)+1
+
+    return dict1==dict2
+
+
+print(isAnagram('abnc', 'abcn'))
+print(isAnagram1('abnc', 'abdc'))
