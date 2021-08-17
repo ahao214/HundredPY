1935dad394b061e72a3678b9dbe737d0e8495e4c
ahao214 <haochen214@163.com>
2021-08-18 07:39:46 +0800

    树

--- Algorithm/tree.py
+++ Algorithm/tree.py
@@ -21,10 +21,20 @@ class FileSystemTree:
         self.now.children.append(node)
         node.parent = self.now
 
-    def ls(self, name):
+    def ls(self):
         return self.now.children
 
-    # def cd(self, name):
+    def cd(self, name):
+        if name[-1] != "/":
+            name += "/"
+        if name == "../":
+            self.now = self.now.parent
+            return
+        for child in self.now.children:
+            if child.name == name:
+                self.now = child
+                return
+        raise ValueError("invalid dir")
 
 
 tree = FileSystemTree()
