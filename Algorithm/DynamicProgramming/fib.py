a0aa96db1bbe9527e6f38f06ab42ddbbd2eb4bd5
ahao214 <haochen214@163.com>
2021-08-19 17:56:52 +0800

    动态规划

--- Algorithm/DynamicProgramming/fib.py
+++ Algorithm/DynamicProgramming/fib.py
@@ -5,15 +5,15 @@ def fibnacci(n):
     if n == 1 or n == 2:
         return 1
     else:
-        return fibnacci(n-1)+fibnacci(n-2)
+        return fibnacci(n - 1) + fibnacci(n - 2)
 
 
 # 非递归
-# 动态规划(DP)的思想
+# 动态规划(DP)的思想 = 递推式 + 重复子问题
 def fibnacci_no(n):
-    f=[0,1,1]
-    if n>2:
-        for i in range(n-2):
+    f = [0, 1, 1]
+    if n > 2:
+        for i in range(n - 2):
             num = f[-1] + f[-2]
             f.append(num)
     return f[n]
