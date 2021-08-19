0853a6e341791c3f163cde255266b8efc63ec61c
ahao214 <haochen214@163.com>
2021-08-19 21:11:31 +0800

    钢条切割问题

--- Algorithm/DynamicProgramming/cut_rob.py
+++ Algorithm/DynamicProgramming/cut_rob.py
@@ -55,3 +55,33 @@ def cut_rob_db(p, n):
 
 
 print(cut_rob_db(p, 9))
+
+
+def cut_rob_extend(p, n):
+    r = [0]
+    s = [0]
+    for i in range(1, n + 1):
+        res_r = 0  # 价格的最大值
+        res_s = 0  # 价格最大值对应方案的左边不切割部分的长度
+        for j in range(1, i + 1):
+            if p[j] + r[i - j] > res_r:
+                res_r = p[j] + r[i - j]
+                res_s = j
+        r.append(res_r)
+        s.append(res_s)
+    return r[n], s
+
+
+print(cut_rob_extend(p, 9))
+
+
+def cut_rob_solution(p, n):
+    r, s = cut_rob_extend(p, n)
+    ans = []
+    while n > 0:
+        ans.append(s[n])
+        n -= s[n]
+    return ans
+
+
+print(cut_rob_solution(p, 7))
