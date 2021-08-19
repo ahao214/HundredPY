927ececc075313f085f1eea31fc215e992b9d0b7
ahao214 <haochen214@163.com>
2021-08-19 17:54:43 +0800

    动态规划

---
+++ Algorithm/DynamicProgramming/fib.py
@@ -0,0 +1,22 @@
+# 斐波那锲
+
+# 递归
+def fibnacci(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fibnacci(n-1)+fibnacci(n-2)
+
+
+# 非递归
+# 动态规划(DP)的思想
+def fibnacci_no(n):
+    f=[0,1,1]
+    if n>2:
+        for i in range(n-2):
+            num = f[-1] + f[-2]
+            f.append(num)
+    return f[n]
+
+
+print(fibnacci_no(3))
