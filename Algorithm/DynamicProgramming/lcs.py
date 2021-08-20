a3d7ec7dc20c03c3b3c6cc1ad22c492986acdbba
ahao214 <haochen214@163.com>
2021-08-20 09:32:02 +0800

    最长公共子序列

--- Algorithm/DynamicProgramming/lcs.py
+++ Algorithm/DynamicProgramming/lcs.py
@@ -4,14 +4,38 @@
 def lcs_length(x, y):
     m = len(x)
     n = len(y)
-    c = [[0 for _ in range(n+1)] for _ in range(m+1)]
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if x[i-1] == y[j-1]:
-                c[i][j] = c[i-1][j-1] + 1
+    c = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if x[i - 1] == y[j - 1]:
+                c[i][j] = c[i - 1][j - 1] + 1
             else:
-                c[i][j] = max(c[i-1][j], c[i][j-1])
+                c[i][j] = max(c[i - 1][j], c[i][j - 1])
     return c[m][n]
 
 
-print(lcs_length("ABCBDAB", "BDCABA"))
+# print(lcs_length("ABCBDAB", "BDCABA"))
+
+
+def lcs(x, y):
+    m = len(x)
+    n = len(y)
+    c = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    b = [[0 for _ in range(n + 1)] for _ in range(m + 1)]  # 1 左上方 2 上方 3 左方
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if x[i - 1] == y[j - 1]:  # i j位置上的字符匹配的时候，来自左上方+1
+                c[i][j] = c[i - 1][j - 1] + 1
+                b[i][j] = 1
+            elif c[i - 1][j] > c[i][j - 1]:  # 来自上方
+                c[i][j] = c[i - 1][j]
+                b[i][j] = 2
+            else:
+                c[i][j] = c[i][j - 1]
+                b[i][j] = 3
+    return c[m][n], b
+
+
+c,b = lcs("ABCBDAB", "BDCABA")
+for _ in b:
+    print(_)
