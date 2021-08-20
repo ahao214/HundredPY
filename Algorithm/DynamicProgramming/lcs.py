e14423d210210280bbbd97d084bd0d36666d0e54
ahao214 <haochen214@163.com>
2021-08-20 09:19:24 +0800

    最长公共子序列

---
+++ Algorithm/DynamicProgramming/lcs.py
@@ -0,0 +1,17 @@
+# 最长公共子序列
+
+
+def lcs_length(x, y):
+    m = len(x)
+    n = len(y)
+    c = [[0 for _ in range(n+1)] for _ in range(m+1)]
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if x[i-1] == y[j-1]:
+                c[i][j] = c[i-1][j-1] + 1
+            else:
+                c[i][j] = max(c[i-1][j], c[i][j-1])
+    return c[m][n]
+
+
+print(lcs_length("ABCBDAB", "BDCABA"))
