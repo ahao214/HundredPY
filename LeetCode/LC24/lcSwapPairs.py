4d5ff9d08f77cb88747b205b7ae0cdd9c438d019
ahao214 <haochen214@163.com>
2021-09-17 17:31:28 +0800

    24. 两两交换链表中的节点

---
+++ LeetCode/LC24/lcSwapPairs.py
@@ -0,0 +1,27 @@
+# 24. 两两交换链表中的节点
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+
+        res = ListNode()
+        res.next = head
+        cur = res
+        while cur.next is not None and cur.next.next is not None:
+            nxt = head.next
+            tmp = nxt.next
+            cur.next = nxt
+            nxt.next = head
+            head.next = tmp
+            cur = head
+            head = head.next
+        return res.next
