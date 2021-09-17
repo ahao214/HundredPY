ae4906957a1cf3c183cd8e66e7f627cccba68f5b
ahao214 <haochen214@163.com>
2021-09-17 17:04:14 +0800

    21. 合并两个有序链表

--- LeetCode/LC21/lcMergeTwoList.py
+++ LeetCode/LC21/lcMergeTwoList.py
@@ -3,7 +3,7 @@
 
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, val=0, next = None):
+    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
@@ -13,8 +13,8 @@ class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         res = ListNode()
         cur = res
-        while(l1 != None and l2 != None):
-            if l1.val<=l2.val:
+        while (l1 != None and l2 != None):
+            if l1.val <= l2.val:
                 cur.next = l1
                 l1 = l1.next
             else:
@@ -23,3 +23,13 @@ class Solution:
             cur = cur.next
         cur.next = l1 or l2
         return res.next
+
+    def mergeTwoList(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 == None or l2 == None:
+            return l1 or l2
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoList(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoList(l1, l2.next)
+            return l2
