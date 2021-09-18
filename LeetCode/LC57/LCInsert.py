67a941b15b51dfbf933c61a35efa85ed84d441b8
ahao214 <haochen214@163.com>
2021-09-18 17:33:36 +0800

    57. 插入区间

--- LeetCode/LC57/LCInsert.py
+++ LeetCode/LC57/LCInsert.py
@@ -17,3 +17,28 @@ def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[Lis
             result[-1][1] = max(result[-1][1], interval[1])
 
     return result
+
+
+# 贪心法
+def insert2(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    result = []
+    index = 0
+    for interval in intervals:
+        if interval[0] < newInterval[0]:
+            result.append(interval)
+            index += 1
+        else:
+            break
+
+        if len(result) == 0 or result[-1][1] < newInterval[0]:
+            result.append(newInterval)
+        else:
+            result[-1][1] = max(result[-1][1], newInterval[1])
+
+        for i in range(index, len(intervals)):
+            if result[-1][1] < intervals[i][0]:
+                result.append(intervals[i])
+            else:
+                result[-1][1] = max(result[-1][1], intervals[i][1])
+
+        return result
